96b91e21548d7754b41721ed2a44310c81f63b9f
ibrahimmt11 <abitmulyo@gmail.com>
2023-04-17 00:35:34 +0700

    menambahkan API mahasiswa

---
+++ tpmodul9/Controllers/mahasiswa.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class mahasiswa : ControllerBase
+    {
+        public static List<Mahasiswa> dataMahasiswa = new List<Mahasiswa>()
+        {
+            new Mahasiswa("Ibrahim", "1302210022"),
+            new Mahasiswa("Arya", "1302213069"),
+            new Mahasiswa("Ridwan", "1302210076"),
+            new Mahasiswa("Hilmi", "1302210081"),
+            new Mahasiswa("Marsya", "1302213003")
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return dataMahasiswa;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return dataMahasiswa[id];
+        }
+
+        [HttpPost]
+        public void Post(Mahasiswa value)
+        {
+            dataMahasiswa.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Mahasiswa value)
+        {
+            dataMahasiswa[id] = value;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            dataMahasiswa.RemoveAt(id);
+        }
+    }
+}
